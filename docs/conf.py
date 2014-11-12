4a0c07e3597e2f9b409fd1758ef17b75fe40f3c2
indorilftw <vgerak@gmail.com>
2014-11-12 11:42:21 +0200

    PEP8 fixes on conf directory

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys
+import os
 
 sys.path.insert(0, os.path.abspath('../snf-cyclades-app'))
 import synnefo
@@ -20,34 +21,34 @@ exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 html_theme = 'default'
 html_theme_options = {
-	'sidebarwidth': '300',
-	'collapsiblesidebar': 'true',
-	'footerbgcolor':    '#55b577',
-	'footertextcolor':  '#000000',
-	'sidebarbgcolor':   '#ffffff',
-	'sidebarbtncolor':  '#f2f2f2',
-	'sidebartextcolor': '#000000',
-	'sidebarlinkcolor': '#328e4a',
-	'relbarbgcolor':    '#55b577',
-	'relbartextcolor':  '#ffffff',
-	'relbarlinkcolor':  '#ffffff',
-	'bgcolor':          '#ffffff',
-	'textcolor':        '#000000',
-	'headbgcolor':      '#ffffff',
-	'headtextcolor':    '#000000',
-	'headlinkcolor':    '#c60f0f',
-	'linkcolor':        '#328e4a',
-	'visitedlinkcolor': '#63409b',
-	'codebgcolor':      '#eeffcc',
-	'codetextcolor':    '#333333'
+    'sidebarwidth': '300',
+    'collapsiblesidebar': 'true',
+    'footerbgcolor':    '#55b577',
+    'footertextcolor':  '#000000',
+    'sidebarbgcolor':   '#ffffff',
+    'sidebarbtncolor':  '#f2f2f2',
+    'sidebartextcolor': '#000000',
+    'sidebarlinkcolor': '#328e4a',
+    'relbarbgcolor':    '#55b577',
+    'relbartextcolor':  '#ffffff',
+    'relbarlinkcolor':  '#ffffff',
+    'bgcolor':          '#ffffff',
+    'textcolor':        '#000000',
+    'headbgcolor':      '#ffffff',
+    'headtextcolor':    '#000000',
+    'headlinkcolor':    '#c60f0f',
+    'linkcolor':        '#328e4a',
+    'visitedlinkcolor': '#63409b',
+    'codebgcolor':      '#eeffcc',
+    'codetextcolor':    '#333333'
 }
 
 htmlhelp_basename = 'synnefodoc'
 
 intersphinx_mapping = {
-        'python': ('http://docs.python.org/', None),
-        'django': ('https://docs.djangoproject.com/en/dev/',
-                   'https://docs.djangoproject.com/en/dev/_objects/')
+    'python': ('http://docs.python.org/', None),
+    'django': ('https://docs.djangoproject.com/en/dev/',
+               'https://docs.djangoproject.com/en/dev/_objects/')
 }
 
 SYNNEFO_PROJECTS = ['synnefo', 'archipelago', 'kamaki', 'snf-image',
